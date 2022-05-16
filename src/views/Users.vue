3d793ab2d8681c105145f9db416728bf8398668e
carla-ng <carlitashata@gmail.com>
2022-05-16 22:21:12 +0200

    Users view and useUsers composable.

---
+++ src/views/Users.vue
@@ -0,0 +1,62 @@
+<template>
+    <h2 v-if="isLoading">Please wait...</h2>
+    <h2 v-else>Users</h2>
+    <h5 v-if="errorMessage">{{ errorMessage }}</h5>
+
+    <div v-if="users.length > 0">
+        <ul>
+            <li v-for="{ first_name, last_name, email, id } in users" :key="id">
+                <h4>{{ first_name }} {{ last_name }}</h4>
+                <h6>{{ email }}</h6>
+            </li>
+        </ul>
+    </div>
+
+    <button @click="prevPage">Prev</button>
+    <button @click="nextPage">Next</button>
+    <span>Page: {{ currentPage }}</span>
+</template>
+
+
+<script>
+import useUsers from '@/composables/useUsers'
+
+export default {
+    setup() {
+        const { currentPage, errorMessage, isLoading, users, nextPage, prevPage } = useUsers()
+
+        return {
+            currentPage,
+            errorMessage,
+            isLoading,
+            users,
+
+            nextPage, 
+            prevPage
+
+            //...useUsers()
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+h2 {
+    text-align: center;
+    width: 100%;
+}
+
+div {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
+
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    width: 250px;
+}
+</style>
