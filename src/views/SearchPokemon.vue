e48aa343b96c303f6ae4587a3b31755a574cfc3e
carla-ng <carlitashata@gmail.com>
2022-05-18 00:24:33 +0200

    Router in setup.

---
+++ src/views/SearchPokemon.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1>Search Pokémon: {{ pokemonId }}</h1>
+    <form @submit.prevent="onSubmit">
+        <input type="number"
+               placeholder="Pokémon number"
+               autofocus
+               v-model="pokemonId"
+        >
+    </form>
+
+    <br>
+
+    <span>Press Enter to search</span>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+    setup() {
+
+        const router = useRouter()
+
+        const pokemonId = ref(1)
+        
+
+        return {
+            pokemonId,
+
+            onSubmit: () => {
+                //console.log(pokemonId.value)
+                router.push({ name: 'pokemon-id', params: { id: pokemonId.value } })
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
